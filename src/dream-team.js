f2f4d3e31a276292776b47d882c84b280b68f72f
Mari525 <mmuuzzaa2011@ya.ru>
2022-04-29 23:04:22 +0300

    feat: add solution to getCommonChara task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam(team) {
+  // throw new NotImplementedError('Not implemented');
+  let name = [];
+  let team2;
+  for (let i = 0; i < team.length; i++) {
+    if (typeof team[i] === 'string') {
+      team2 = team[i].trim();
+      name.push(team2[0].toUpperCase());
+      // name.push(team[i].trim().team[i][0].toUpperCase());
+    }
+  }
+  return name.sort().join('');
 }
 
 module.exports = {
